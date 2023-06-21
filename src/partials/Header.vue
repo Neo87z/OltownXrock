7d1d016d725834d48d9ac236b2008918ec075929
Neo87z <shehanhoradagoda12@gmail.com>
2023-06-21 08:44:58 -0230

    'main'

--- src/partials/Header.vue
+++ src/partials/Header.vue
@@ -17,7 +17,7 @@
           <!-- Desktop sign in links -->
           <ul class="flex grow justify-end flex-wrap items-center">
 
-            <li v-if="localStorageVariablePresent" class="ml-6">
+            <li class="ml-6">
               <router-link
                 class="btn-sm text-slate-300 hover:text-white transition duration-150 ease-in-out w-full group [background:linear-gradient(theme(colors.slate.900),_theme(colors.slate.900))_padding-box,_conic-gradient(theme(colors.slate.400),_theme(colors.slate.700)_25%,_theme(colors.slate.700)_75%,_theme(colors.slate.400)_100%)_border-box] relative before:absolute before:inset-0 before:bg-slate-800/30 before:rounded-full before:pointer-events-none"
                 to="/signup">
